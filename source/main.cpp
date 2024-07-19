40f5174bed371fbb4409542be3a60d3db6fcf193
Alon Sirota <sirotaalon@gmail.com>
2024-07-19 20:14:29 +0300

    add perspective projection

--- source/main.cpp
+++ source/main.cpp
@@ -1,21 +1,23 @@
 #include <GL/freeglut_std.h>
+#include <GL/glu.h>
 #include <GL/glut.h>
 
+void setupCamera();
 void displayFunc();
 void displayHead();
 void drawAxis();
 
 void drawAxis() {
   glBegin(GL_LINES);
-  // x-axis (red)
-  glColor3f(1, 0, 0);
-  glVertex3f(0, 0, 0);
-  glVertex3f(5, 0, 0);
-
   // y-axis (green)
   glColor3f(0, 1, 0);
   glVertex3f(0, 0, 0);
   glVertex3f(0, 5, 0);
+
+  // x-axis (red)
+  glColor3f(1, 0, 0);
+  glVertex3f(0, 0, 0);
+  glVertex3f(5, 0, 0);
 
   // z-axis (blue)
   glColor3f(0, 0, 1);
@@ -24,22 +26,29 @@ void drawAxis() {
   glEnd();
 }
 
-void setupOrthographicProjection() {
+void setupProjection() {
   glMatrixMode(GL_PROJECTION);
   glLoadIdentity();
-  glOrtho(-2, 2, -2, 2, -2, 2);
+  gluPerspective(45, 1, 1, 100);
+}
+
+void setupCamera() {
+  glMatrixMode(GL_MODELVIEW);
+  gluLookAt(0, 0, 10, 0, 0, 1, 0, 1, 0);
 }
 
 void displayFunc() {
   glClear(GL_COLOR_BUFFER_BIT);
-  setupOrthographicProjection();
+  setupCamera();
+  setupProjection();
   drawAxis();
   displayHead();
   glFlush();
 }
 
 void displayHead() {
-  glutSolidTeapot(1);
+  glMatrixMode(GL_MODELVIEW);
+  glutSolidTetrahedron();
 }
 
 int main(int argc, char **argv) {
