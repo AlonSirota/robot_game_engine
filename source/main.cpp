de4941e8e5dcaa8f4c6a29837c16fbcb5ebd624a
Alon Sirota <sirotaalon@gmail.com>
2024-07-20 02:11:40 +0300

    add state and delta time infrastructure with forward movement

--- source/main.cpp
+++ source/main.cpp
@@ -1,11 +1,36 @@
 #include <GL/freeglut_std.h>
 #include <GL/glu.h>
 #include <GL/glut.h>
+#include <cstdio>
 
+#define ROBOT_SPEED 1
+
+void clearMatrices();
 void setupCamera();
 void displayFunc();
 void displayHead();
 void drawAxis();
+void keyboardFunc(unsigned char key, int x, int y);
+void keyboardUpFunc(unsigned char key, int x, int y);
+void idleFunc();
+
+struct Point {
+  float x;
+  float y;
+  float z;
+};
+
+struct State {
+  bool isMovingForward;
+  float presentedTime;
+  Point robotPosition;
+
+  State(bool isMovingForward, float presentedTime, Point robotPosition)
+      : isMovingForward(isMovingForward), presentedTime(presentedTime),
+        robotPosition(robotPosition) {}
+};
+
+static State state = State(false, 0, Point{0, 0, 0});
 
 void drawAxis() {
   glBegin(GL_LINES);
@@ -39,6 +64,7 @@ void setupCamera() {
 
 void displayFunc() {
   glClear(GL_COLOR_BUFFER_BIT);
+  clearMatrices();
   setupCamera();
   setupProjection();
   drawAxis();
@@ -46,9 +72,43 @@ void displayFunc() {
   glFlush();
 }
 
+void clearMatrices() {
+  glMatrixMode(GL_MODELVIEW);
+  glLoadIdentity();
+  glMatrixMode(GL_PROJECTION);
+  glLoadIdentity();
+}
+
 void displayHead() {
   glMatrixMode(GL_MODELVIEW);
+  glPushMatrix();
+  glTranslatef(state.robotPosition.x, state.robotPosition.y, state.robotPosition.z);
   glutSolidTetrahedron();
+  glPopMatrix();
+}
+
+void keyboardFunc(unsigned char key, int x, int y) {
+  if (key == 'w') {
+    state.isMovingForward = true;
+  }
+}
+
+void keyboardUpFunc(unsigned char key, int x, int y) {
+  if (key == 'w') {
+    state.isMovingForward = false;
+  }
+}
+
+void idleFunc() {
+  float currentTime = glutGet(GLUT_ELAPSED_TIME) / 1000.0;
+  float deltaTime = currentTime - state.presentedTime;
+
+  if (state.isMovingForward) {
+    state.robotPosition.y += ROBOT_SPEED * deltaTime;
+  }
+
+  state.presentedTime = currentTime;
+  glutPostRedisplay();
 }
 
 int main(int argc, char **argv) {
@@ -57,6 +117,9 @@ int main(int argc, char **argv) {
   glutCreateWindow("Final project.");
 
   glutDisplayFunc(displayFunc);
+  glutIdleFunc(idleFunc);
+  glutKeyboardFunc(keyboardFunc);
+  glutKeyboardUpFunc(keyboardUpFunc);
 
   glutMainLoop();
   return 0;
