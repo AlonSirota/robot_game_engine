339f35ef8ec4425217cd24344ebfde2fdf3623a4
Ram Ben <ramben13@gmail.com>
2024-08-23 19:20:16 +0300

    fix uint again :3

--- source/ui/SolidColor.cpp
+++ source/ui/SolidColor.cpp
@@ -29,7 +29,7 @@ bool SolidColor::MouseClick(double mX, double mY){
     return isInside;
 }
 
-void DrawCircle(float x, float y, float radios, float width, double borderColor[], double infilColor[], uint samples){
+void DrawCircle(float x, float y, float radios, float width, double borderColor[], double infilColor[], unsigned int samples){
     //begin drawing circle 
     glColor4d(borderColor[0], borderColor[1], borderColor[2], borderColor[3]);
     glLineWidth(width);
