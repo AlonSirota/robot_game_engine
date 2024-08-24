0ba86cd1756418e1214968a2ee222fa5f0ee8eb1
Alon Sirota <sirotaalon@gmail.com>
2024-08-25 00:18:44 +0300

    adding comments

--- source/State.h
+++ source/State.h
@@ -12,13 +12,36 @@
 #define RENDER_HIGHT 1080
 #define RENDER_ASPECT_RATIO ((float)RENDER_WIDTH / (float)RENDER_HIGHT)
 
+// The object that is currenlty being controlled, for example, if ControlMode is
+// Hand, pressing the arrow keys will rotate the Hand.
 enum ControlMode { Robot, RobotHead, UpperArm, LowerArm, Hand, Camera, Lamp };
+
+/// The point of view of the camera, when FirstPerson, the camera is positioned
+/// at the head of the robot, and in third person the camera is positioned
+/// according the the Camera position in the state.
 enum PointOfView { FirstPerson, ThirdPerson };
 
-enum ActiveMenue { None, Main, Help, Options };
+// The current menu shown to the user.
+enum ActiveMenue {
+  // No menu, the scene is interactable with the keyboard.
+  None,
 
+  // The main UI menu, allowing navigations to the rest of the menus.
+  Main,
+
+  // The help screen, showing instructions on how to interact with the program.
+  Help,
+
+  // Options menu, allowing configuring the different light source.
+  Options
+};
+
+// Models a position and rotation of an object in the scene.
 struct Transform {
+  // position of the object in world cooridinates.
   Point position;
+
+  // The rotation of the object.
   Quaternion quaternion;
 
   Transform(Point position, Quaternion quaternion)
@@ -26,11 +49,7 @@ struct Transform {
 };
 
 struct Robot {
-  Robot(Transform transform, Quaternion headRotation, Quaternion armRotation,
-        Quaternion elbowRotation, Quaternion handRotation)
-      : transform(transform), headRotationRelativeToTransform(headRotation),
-        armRotation(armRotation), elbowRotation(elbowRotation),
-        handRotation(handRotation) {}
+  // transform of the robot.
   Transform transform;
 
   // This rotation is relative to the torso.
@@ -44,8 +63,15 @@ struct Robot {
 
   // Relative to the hand pivot point.
   Quaternion handRotation;
+
+  Robot(Transform transform, Quaternion headRotation, Quaternion armRotation,
+        Quaternion elbowRotation, Quaternion handRotation)
+      : transform(transform), headRotationRelativeToTransform(headRotation),
+        armRotation(armRotation), elbowRotation(elbowRotation),
+        handRotation(handRotation) {}
 };
 
+/// The possible keyboard commands that are currently active.
 struct ControlCommands {
   bool isMovingForward;
   bool isMovingBackward;
@@ -72,27 +98,43 @@ struct ViewportInfo {
 };
 
 struct State {
+  // the current point of view of the camera.
   PointOfView pointOfView;
+
+  // The current mode of control, for example, if the control mode is Robot, the
+  // arrow keys will move the robot.
   ControlMode controlMode;
+
+  // The current keyboard commands that are active.
   ControlCommands controlCommands;
+
+  // The current state of the robot in the
   struct Robot robot;
+
+  // The current state of the camera.
   Transform camera;
+
   int windowHeight;
   int windowWidth;
 
   ViewportInfo viewportInfo;
 
+  // true if and only if the menu is currently being shown.
   bool showingMenu;
 
+  // true if and only if the debug info should be shown.
   bool displayDebugInfo;
 
+  // The current active menu.
   ActiveMenue activeMenue;
 
+  // Current color and intensity of the global ambient light source.
   GLint AmbientR;
   GLint AmbientG;
   GLint AmbientB;
   GLint AmbientI;
 
+  // current color, intensity and position of the lamp (the point light source).
   GLint PointLightR;
   GLint PointLightG;
   GLint PointLightB;
@@ -111,69 +153,26 @@ struct State {
         lampPosition({-5, 3, 0}, {1, 0, 0}) {}
 };
 
-inline void moveForwardAndBackwords(Transform &t,
-                                    ControlCommands controlCommands,
-                                    bool shouldInvertForwardDirection,
-                                    double deltaTime) {
-  Point forward =
-      t.quaternion.getForwardVector() * (shouldInvertForwardDirection ? -1 : 1);
+void updatedState(State &currentState, double deltaTime);
+void moveForwardAndBackwords(Transform &t, ControlCommands controlCommands,
+                             bool shouldInvertForwardDirection,
+                             double deltaTime);
+void moveLeftAndRight(Transform &t, const ControlCommands &controlCommands,
+                      bool shouldInvertDirection, double deltaTime);
+void moveUpAndDown(Transform &t, const ControlCommands &controlCommands,
+                   double deltaTime);
+void rotateLeftAndRight(Quaternion &quaternion,
+                        const ControlCommands &controlCommands,
+                        double deltaTime, double rotationSpeed);
+void rotateUpAndDown(Quaternion &quaternion,
+                     const ControlCommands &controlCommands, double deltaTime,
+                     double rotationSpeed);
 
-  if (controlCommands.isMovingForward) {
-    t.position += forward * MOVE_SPEED * deltaTime;
-  } else if (controlCommands.isMovingBackward) {
-    t.position -= forward * MOVE_SPEED * deltaTime;
-  }
-}
-
-inline void moveLeftAndRight(Transform &t,
-                             const ControlCommands &controlCommands,
-                             bool shouldInvertDirection, double deltaTime) {
-  Point right =
-      t.quaternion.getRightVector() * (shouldInvertDirection ? -1 : 1);
-  if (controlCommands.isMovingLeft) {
-    t.position += right * MOVE_SPEED * deltaTime;
-  } else if (controlCommands.isMovingRight) {
-    t.position -= right * MOVE_SPEED * deltaTime;
-  }
-}
-
-inline void moveUpAndDown(Transform &t, const ControlCommands &controlCommands,
-                          double deltaTime) {
-  if (controlCommands.isMovingUp) {
-    t.position += t.quaternion.getUpVector() * MOVE_SPEED * deltaTime;
-  } else if (controlCommands.isMovingDown) {
-    t.position -= t.quaternion.getUpVector() * MOVE_SPEED * deltaTime;
-  }
-}
-
-inline void rotateLeftAndRight(Quaternion &quaternion,
-                               const ControlCommands &controlCommands,
-                               double deltaTime, double rotationSpeed) {
-  Point up = quaternion.getUpVector() * 1;
-  if (controlCommands.isRotatingLeft) {
-    Quaternion rotation(rotationSpeed * deltaTime, up);
-    quaternion = (rotation.normalize() * quaternion.normalize()).normalize();
-  }
-  if (controlCommands.isRotatingRight) {
-    Quaternion rotation(-rotationSpeed * deltaTime, up);
-    quaternion = (rotation.normalize() * quaternion.normalize()).normalize();
-  }
-}
-
-inline void rotateUpAndDown(Quaternion &quaternion,
-                            const ControlCommands &controlCommands,
-                            double deltaTime, double rotationSpeed) {
-  Point right = quaternion.getRightVector();
-  if (controlCommands.isRotatingUp) {
-    Quaternion rotation(rotationSpeed * deltaTime, right);
-    quaternion = (rotation.normalize() * quaternion.normalize()).normalize();
-  }
-  if (controlCommands.isRotatingDown) {
-    Quaternion rotation(-rotationSpeed * deltaTime, right);
-    quaternion = (rotation.normalize() * quaternion.normalize()).normalize();
-  }
-}
-
+// Updates the state of the scene according to the current control commands.
+// The control mode controls which part of the scene is currently being
+// manipulated, some objects can be manipulated in different ways, for example -
+// the robot head can only rotate, while the robot itself can move and also
+// rotate.
 inline void updatedState(State &currentState, double deltaTime) {
   switch (currentState.controlMode) {
   case Robot:
@@ -231,5 +230,74 @@ inline void updatedState(State &currentState, double deltaTime) {
   }
   currentState.windowHeight = glutGet(GLUT_WINDOW_HEIGHT);
   currentState.windowWidth = glutGet(GLUT_WINDOW_WIDTH);
+}
+
+// The following functions are used to move and rotate the different objects in
+// the scene, using the delta time and speed to calculate by how much to update.
+
+inline void moveForwardAndBackwords(Transform &t,
+                                    ControlCommands controlCommands,
+                                    bool shouldInvertForwardDirection,
+                                    double deltaTime) {
+  // We expose the shouldInvertForwardDirection because we want to invert the
+  // controls when controlling the camera, because the camera looks at the
+  // neagtive z axis, making controls feel "backwords" if we don't invert them.
+  Point forward =
+      t.quaternion.getForwardVector() * (shouldInvertForwardDirection ? -1 : 1);
+
+  if (controlCommands.isMovingForward) {
+    t.position += forward * MOVE_SPEED * deltaTime;
+  } else if (controlCommands.isMovingBackward) {
+    t.position -= forward * MOVE_SPEED * deltaTime;
+  }
+}
+
+inline void moveLeftAndRight(Transform &t,
+                             const ControlCommands &controlCommands,
+                             bool shouldInvertDirection, double deltaTime) {
+  Point right =
+      t.quaternion.getRightVector() * (shouldInvertDirection ? -1 : 1);
+  if (controlCommands.isMovingLeft) {
+    t.position += right * MOVE_SPEED * deltaTime;
+  } else if (controlCommands.isMovingRight) {
+    t.position -= right * MOVE_SPEED * deltaTime;
+  }
+}
+
+inline void moveUpAndDown(Transform &t, const ControlCommands &controlCommands,
+                          double deltaTime) {
+  if (controlCommands.isMovingUp) {
+    t.position += t.quaternion.getUpVector() * MOVE_SPEED * deltaTime;
+  } else if (controlCommands.isMovingDown) {
+    t.position -= t.quaternion.getUpVector() * MOVE_SPEED * deltaTime;
+  }
+}
+
+inline void rotateLeftAndRight(Quaternion &quaternion,
+                               const ControlCommands &controlCommands,
+                               double deltaTime, double rotationSpeed) {
+  Point up = quaternion.getUpVector() * 1;
+  if (controlCommands.isRotatingLeft) {
+    Quaternion rotation(rotationSpeed * deltaTime, up);
+    quaternion = (rotation.normalize() * quaternion.normalize()).normalize();
+  }
+  if (controlCommands.isRotatingRight) {
+    Quaternion rotation(-rotationSpeed * deltaTime, up);
+    quaternion = (rotation.normalize() * quaternion.normalize()).normalize();
+  }
+}
+
+inline void rotateUpAndDown(Quaternion &quaternion,
+                            const ControlCommands &controlCommands,
+                            double deltaTime, double rotationSpeed) {
+  Point right = quaternion.getRightVector();
+  if (controlCommands.isRotatingUp) {
+    Quaternion rotation(rotationSpeed * deltaTime, right);
+    quaternion = (rotation.normalize() * quaternion.normalize()).normalize();
+  }
+  if (controlCommands.isRotatingDown) {
+    Quaternion rotation(-rotationSpeed * deltaTime, right);
+    quaternion = (rotation.normalize() * quaternion.normalize()).normalize();
+  }
 }
 #endif
